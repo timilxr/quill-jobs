67ba3e0b0085db0d6fe9cce71aeee7f18067fdae
timilxr <38140090+timilxr@users.noreply.github.com>
2026-03-12 16:36:26 +0100

    feat: Loading Spinner

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import JobListing from './JobListing'
 import type { IJob } from '../util/models';
+import Spinner from './Spinner';
 
 const JobListings = ({ isHome = false }: { isHome?: boolean }) => {
     const [jobs, setJobs] = useState<IJob[]>([]);
@@ -28,17 +29,17 @@ const JobListings = ({ isHome = false }: { isHome?: boolean }) => {
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                     {isHome ? "Recent" : "Browse"} Jobs
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {/* <!-- Job Listing 1 --> */}
-                    {
-                        loading ?
-                            <h1>Loading...</h1>
-                            :
-                            jobs.map(job => (
+                {
+                    loading ?
+                        <Spinner loading={loading} />
+                        :
+                        (<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                            {/* <!-- Job Listing 1 --> */}
+                            {jobs.map(job => (
                                 <JobListing key={job.id} job={job} />
-                            ))
-                    }
-                </div>
+                            ))}
+                        </div>)
+                }
             </div>
         </section>
     )
